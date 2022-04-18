e5372dbc226636f61dfd9426d56b6f3ee9680010
Nikolai Nachev <nikolaidan1@abv.bg>
2022-04-18 09:05:33 +0300

    Add files via upload

---
+++ IntroToCSharp/IntroToCSharp/HelloCSharp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace IntroToCSharp
+{
+    class HelloCSharp
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter your fucking name here :");
+            String Name = Console.ReadLine();
+            // for(int x=0;x<5;x++)
+            //{
+            Console.Beep();
+            //}
+
+            Console.WriteLine("Your name is : " +
+                Name +
+                "\r\n Is that right?(Y/N)");
+            bool confirmed = false;
+            ConsoleKey response;
+            do
+            {
+                Console.Write("Are you sure you want to choose this as your login key? [y/n] ");
+                response = Console.ReadKey(false).Key;   // true is intercept key (dont show), false is show
+                if (response != ConsoleKey.Enter)
+                    Console.WriteLine();
+
+            } while (response != ConsoleKey.Y && response != ConsoleKey.N);
+            {
+
+                confirmed = response == ConsoleKey.Y;
+            }
+            while (!confirmed) ;
+            {
+                Console.WriteLine("You chose {0}!", Name);
+                Console.ReadLine();
+            }
+        }
+    }
+} 
